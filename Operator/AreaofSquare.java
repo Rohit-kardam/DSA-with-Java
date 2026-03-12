0a6a5c817828fb9ef04accb3f0a21b65db07987c
Rohit kardam <kardamrohit9@gmail.com>
2026-03-12 18:02:05 +0530

    Add files via upload

---
+++ Operator/AreaofSquare.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class AreaofSquare {
+ public static void main(String[] args) {
+    int a;
+    Scanner sc= new Scanner(System.in);
+    System.out.println("ENTER THE SIDE 0F SQAURE");
+    a= sc.nextInt();
+    System.out.println(" the area of sqaure is"+a*a);
+ }
+    
+
+}
---
+++ Operator/Average.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Average {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter values of a ,b and c:");
+        a=Sc.nextInt();
+        b=Sc.nextInt();
+        c=Sc.nextInt();
+        System.out.println((a+b+c)/3);
+    }
+}
